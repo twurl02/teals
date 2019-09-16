c3e329d2e58e685ed6d4db2d72f5e1e111c6ad16
twurl02 <twurl02@gmail.com>
2019-09-16 12:38:19 -0700

    new file:   Program.cs 	new file:   bin/Debug/netcoreapp2.1/teals.deps.json 	new file:   bin/Debug/netcoreapp2.1/teals.dll 	new file:   bin/Debug/netcoreapp2.1/teals.pdb 	new file:   bin/Debug/netcoreapp2.1/teals.runtimeconfig.dev.json 	new file:   bin/Debug/netcoreapp2.1/teals.runtimeconfig.json 	new file:   obj/Debug/netcoreapp2.1/teals.AssemblyInfo.cs 	new file:   obj/Debug/netcoreapp2.1/teals.AssemblyInfoInputs.cache 	new file:   obj/Debug/netcoreapp2.1/teals.assets.cache 	new file:   obj/Debug/netcoreapp2.1/teals.csproj.CoreCompileInputs.cache 	new file:   obj/Debug/netcoreapp2.1/teals.csproj.FileListAbsolute.txt 	new file:   obj/Debug/netcoreapp2.1/teals.csprojAssemblyReference.cache 	new file:   obj/Debug/netcoreapp2.1/teals.dll 	new file:   obj/Debug/netcoreapp2.1/teals.db 	new file:   obj/project.assets.json 	new file:   obj/teals.csproj.nuget.cache 	new file:   obj/teals.csproj.nuget.dgspec.json 	new file:   obj/teals.csproj.nuget.g.props 	new file:   obj/teals.csproj.nuget.g.targets 	new file:   teals.csproj

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace teals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
